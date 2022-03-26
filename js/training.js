97f74aee69db0f3df813069d868dffdd1d2cd4b8
SergeyKudinov <serezha.kudinov.1995@mail.ru>
2022-03-26 17:46:13 +0300

    FirstProject

---
+++ js/training.js
@@ -0,0 +1,60 @@
+
+
+
+/* class User {
+    constructor (name, id) {
+            this.name = name;
+            this.id = id;
+            this.human = true;
+    }
+    hello() {
+        console.log(`hello! ${this.name}`);
+    }
+    exit() {
+        console.log(`Пользователь ${this.name} вышел`);
+    }
+}
+const sergey = new User('Sergey', 20);
+console.log(sergey); */
+
+/* class Rectangle {
+    constructor(height, width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    calcArea() {
+        return this.height * this.width;
+    }
+}
+
+class coloredRectangleWithText extends Rectangle {
+    constructor(height, width, text, bgColor) {
+        super(height, width);
+        this.text = text;
+        this.bgColor = bgColor;
+    }
+
+    showMyProps() {
+        console.log(`Текст: ${this.text}, цвет: ${this.bgColor}`);
+    }
+
+}
+
+const div = new coloredRectangleWithText(25, 10, 'Hello world', 'red');
+div.showMyProps();
+console.log(div.calcArea()); */
+
+/* const square = new Rectangle(10, 10);
+      long = new Rectangle(20, 100);
+console.log(square.calcArea());
+console.log(long.calcArea()); */
+
+/* const persone = {
+    name: "Alex",
+    tel: '+7424352443'
+};
+console.log(JSON.parse(JSON.stringify(persone))); */
+
+
+
